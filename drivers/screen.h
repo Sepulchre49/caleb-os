56e2679e00c802f11eba89e0de7ff63fb3008ab4
Caleb Fringer <cfringer98@gmail.com>
2023-07-19 11:35:17 -0700

    Added END_OF_SCREEN constant and handle_scrolling prototype

--- drivers/screen.h
+++ drivers/screen.h
@@ -6,6 +6,7 @@
 #define MAX_COLS 80
 // Attribute byte for our default color scheme
 #define WHITE_ON_BLACK 0x0f
+#define END_OF_SCREEN 2*(MAX_ROWS*MAX_COLS)
 
 // Screen device I/O ports (for the cursor)
 #define REG_SCREEN_CTRL 0x3d4
@@ -21,6 +22,7 @@ void print_at(char *message, int row, int col);
 void print(char *message);
 
 void clear_screen();
+int handle_scrolling(int cursor);
 
 int get_offset_row(int offset);
 int get_offset_col(int offset);
