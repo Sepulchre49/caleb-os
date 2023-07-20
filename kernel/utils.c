578655dc3dd2075fcdd1d8ede9642e2003cd79f4
Caleb Fringer <cfringer98@gmail.com>
2023-07-19 17:14:03 -0700

    Implemented memset, now using types defined in cpu/types.h

--- kernel/utils.c
+++ kernel/utils.c
@@ -1,5 +1,13 @@
-void mem_copy(unsigned char *source, unsigned char *dest, int size) {
+#include "utils.h"
+
+void mem_copy(u8int *source, u8int *dest, s32int size) {
     const int MAX_COLS = 80;
     for (int i=0; i<2*MAX_COLS; i++)
         dest[i] = source[i];
+}
+
+void mem_set(u8int *dest, u8int ch, u32int count) {
+    for (int i=0; i<count; i++) {
+        dest[i] = ch;
+    }
 }
--- kernel/utils.h
+++ kernel/utils.h
@@ -1,4 +1,7 @@
+#include "../cpu/types.h"
+
 #ifndef UTILS_H
 #define UTILS_H
-void mem_copy(unsigned char* source, unsigned char* dest, int size);
+void mem_copy(u8int* source, u8int* dest, s32int size);
+void mem_set(u8int* dest, u8int ch, u32int count);
 #endif
