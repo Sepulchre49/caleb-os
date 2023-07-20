8f9aaa7e9e835b213351db5b2242afe643896435
Caleb Fringer <cfringer98@gmail.com>
2023-07-19 17:50:20 -0700

    Syntax issues

--- cpu/idt.h
+++ cpu/idt.h
@@ -3,20 +3,22 @@
 #ifndef IDT_H
 #define IDT_H
 // Describes an interrupt gate
-struct {
+struct idt_entry {
     u16int base_lo;    // Lower 16 bits of interrupt routine address
     u16int sel;        // Kernel segment selector (0x08)
     u8int  always0;    // Must be zero
     u8int  flags;      // See documentation
     u16int base_hi;    // Upper 16 bits of interrupt routine address
-} __atribute__((packed)) idt_entry_t;
+} __attribute__((__packed__));
+typedef struct idt_entry idt_entry_t;
 
 // Special kind of pointer for the `lidt` instruction
 // Describes a pointer to an array of interrupt handlers
-struct {
+struct idt_ptr {
     u16int limit;
     u32int base;
-} __attribute__((packed)) idt_ptr_t;
+} __attribute__((__packed__));
+typedef struct idt_ptr idt_ptr_t;
 
 // Interrupt vector table initializer function
 void init_idt();
