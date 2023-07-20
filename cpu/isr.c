014016132d6480773885e7903fe015ad7eff026f
Caleb Fringer <cfringer98@gmail.com>
2023-07-20 12:19:53 -0700

    Added error messages to interrupt handler

--- cpu/isr.c
+++ cpu/isr.c
@@ -1,6 +1,47 @@
 #include "../drivers/screen.h"
 #include "isr.h"
 
+// List of exception messages
+char *exception_messages[] = {
+    "Division By Zero",
+    "Debug",
+    "Non Maskable Interrupt",
+    "Breakpoint",
+    "Into Detected Overflow",
+    "Out of Bounds",
+    "Invalid Opcode",
+    "No Coprocessor",
+
+    "Double Fault",
+    "Coprocessor Segment Overrun",
+    "Bad TSS",
+    "Segment Not Present",
+    "Stack Fault",
+    "General Protection Fault",
+    "Page Fault",
+    "Unknown Interrupt",
+
+    "Coprocessor Fault",
+    "Alignment Check",
+    "Machine Check",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved"
+};
+
 void isr_handler(registers_t regs) {
-    print("Received interrupt!\n");
+    print("Received interrupt! ");
+    print(exception_messages[regs.int_no]);
+    print("\n");
 }
