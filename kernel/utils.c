ce382297559e8f9e14713bf6d3de06968c9c07f0
Caleb Fringer <cfringer98@gmail.com>
2023-07-21 12:30:42 -0700

    Added K&R int_to_ascii function and helper methods

--- kernel/utils.c
+++ kernel/utils.c
@@ -11,3 +11,37 @@ void mem_set(u8int *dest, u8int ch, u32int count) {
         dest[i] = ch;
     }
 }
+
+/**
+ * K&R implementation
+ */
+void int_to_ascii(int n, char str[]) {
+    int i, sign;
+    if ((sign = n) < 0) n = -n;
+    i = 0;
+    do {
+        str[i++] = n % 10 + '0';
+    } while ((n /= 10) > 0);
+
+    if (sign < 0) str[i++] = '-';
+    str[i] = '\0';
+
+    reverse(str);
+}
+
+/* K&R */
+void reverse(char s[]) {
+    int c, i, j;
+    for (i = 0, j = strlen(s)-1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+/* K&R */
+int strlen(char s[]) {
+    int i = 0;
+    while (s[i] != '\0') ++i;
+    return i;
+}
