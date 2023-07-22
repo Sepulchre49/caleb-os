38995f3b75735a32595b95fe515888a0431ebffa
CalebF98 <cfringer98@gmail.com>
2023-07-21 17:30:47 -0700

    Revert "Added K&R int_to_ascii function and helper methods"
    This reverts commit ce382297559e8f9e14713bf6d3de06968c9c07f0. This is
    the last known kernel version without bugs in the interrupt routines

--- kernel/utils.h
+++ kernel/utils.h
@@ -5,8 +5,4 @@
 
 void mem_copy(u8int* source, u8int* dest, s32int size);
 void mem_set(u8int* dest, u8int ch, u32int count);
-
-void int_to_ascii(int n, char str[]);
-void reverse(char s[]);
-int strlen(char s[]);
 #endif
