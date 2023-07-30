c67fda340100b8c65526304f6680387488b48db1
CalebF98 <cfringer98@gmail.com>
2023-07-30 12:48:29 -0700

    Added bool and true/false to types

--- cpu/types.h
+++ cpu/types.h
@@ -1,7 +1,11 @@
 #ifndef TYPE_H
 #define TYPE_H
 
+#define true  1
+#define false 0
+
 // useful typedefs
+typedef unsigned char  bool;
 typedef unsigned int   u32int;
 typedef          int   s32int;
 typedef unsigned short u16int;
